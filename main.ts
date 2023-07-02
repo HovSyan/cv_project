6fc4821fd5bd163de10f30dddfbec915e4923281
Hovhannes Sahakyan <hovhannes@superannotate.com>
2023-07-02 16:19:32 +0400

    Configured webpack

--- main.ts
+++ main.ts
@@ -1,5 +1,24 @@
+import { Chart, ChartData } from "chart.js";
+
+export const chartData: Record<string, ChartData> = {
+    'superannnotate-chart': {
+        datasets: [{
+            data: [20, 20, 20, 20, 20],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    }
+};
+
+console.log('Hello World!')
+
 window.onload = () => {
     addTimelineDotsListeners();
+    generateChart('superannnotate-chart');
 }
 
 function addTimelineDotsListeners(): void {
@@ -10,4 +29,15 @@ function addTimelineDotsListeners(): void {
             node.classList.add('visited');
         }, { once: true });
     })
+}
+
+function generateChart(id: string): void {
+    const context: HTMLElement | null = document.getElementById(id);
+
+    if(context && context instanceof HTMLCanvasElement) {
+        const chart = new Chart(context, {
+            type: 'doughnut',
+            data: chartData[id]
+        })
+    }
 }
